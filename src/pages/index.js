43cb7d0a5ba7b54760e823fbfeaae5e2956c31d8
oskar <o.haller@isarsoft.com>
2020-05-04 19:48:09 +0200

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,22 @@
 import React from "react"
 import { Link } from "gatsby"
+import { Button } from "gatsby-theme-material-ui";
+
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = () => {
+const anchorRef = React.useRef(null);
+
+return(
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
+	 <Button ref={anchorRef} > test </Button>
+
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
@@ -17,5 +24,6 @@ const IndexPage = () => (
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
+}
 
 export default IndexPage
